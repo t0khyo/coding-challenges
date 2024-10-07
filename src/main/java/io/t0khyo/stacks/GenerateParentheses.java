71d06955720a6408afa1662782258bc3e8edc8ae
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-10-07 23:39:31 +0300

    GenerateParentheses

---
+++ src/main/java/io/t0khyo/stacks/GenerateParentheses.java
@@ -0,0 +1,19 @@
+package io.t0khyo.stacks;
+
+import java.util.List;
+
+public class GenerateParentheses {
+    /**
+     * recursive back tracking approach:
+     * only add open parentheses if open < n
+     * only add closed if closed < open
+     * valid IFF open == close == n
+     */
+    public List<String> generateParenthesis(int n) {
+
+    }
+
+    private void backTrack(int openP, int closedP) {
+
+    }
+}
