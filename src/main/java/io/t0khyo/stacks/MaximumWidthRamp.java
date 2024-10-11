856a4037194f28595166be80f8a88572af2db47c
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-10-11 17:55:05 +0300

    MaximumWidthRamp

--- src/main/java/io/t0khyo/stacks/MaximumWidthRamp.java
+++ src/main/java/io/t0khyo/stacks/MaximumWidthRamp.java
@@ -1,7 +1,32 @@
 package io.t0khyo.stacks;
 
-public class MaximumWidthRamp {
-    public int maxWidthRamp(int[] nums) {
+import java.util.Arrays;
 
+public class MaximumWidthRamp {
+    public static int maxWidthRamp(int[] nums) {
+        int currMax = Integer.MIN_VALUE;
+        int[] maxRight = new int[nums.length];
+        for (int i = nums.length-1;i >= 0;i--) {
+            if (nums[i] > currMax) {
+                currMax = nums[i];
+            }
+            maxRight[i] = currMax;
+        }
+
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0;r < nums.length;r++) {
+            while (nums[l] > maxRight[r]) {
+                l++;
+            }
+            res = Math.max(res, r-l);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        maxWidthRamp(new int[]{6,0,8,3,1,6});
     }
 }
